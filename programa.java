6c8dab72a4c2cf3317cf57a115c4ab0e7ffac36b
vinicios22 <vinicios.grossl@escola.pr.gov.br>
2026-09-23 07:46:42 -0300

    iniciando projeto

--- programa.java
+++ programa.java
@@ -1,8 +1,8 @@
 
-public class Programa {
+public class programa {
    
     public static void main(String args[]) {
-            
+            System.out.println("Olá java");
             
     }
 
